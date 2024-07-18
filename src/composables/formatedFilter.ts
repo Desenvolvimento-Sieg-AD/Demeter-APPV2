fcd5bd78f5ec022fd739b4a64be492ee86cdbde8
cledson96 <cledson1996@gmail.com>
2024-07-18 09:22:22 -0300

    feature:retirando console

--- src/composables/formatedFilter.ts
+++ src/composables/formatedFilter.ts
@@ -7,7 +7,6 @@ export const formatFilter = (filterArray: any) => {
         continue
       }
       if(Array.isArray(filterArray[i][0]) && filterArray[i].length > 2) {
-        console.log('passei aqq 2', filterArray[i])
           for(let j = 0; j < filterArray[i].length; j++){
               if(filterArray[i][j] === 'or') {
                   formattedFilters.push({ fieldName : 'or', value:undefined })
@@ -22,7 +21,6 @@ export const formatFilter = (filterArray: any) => {
       if (filterArray[i] === filterArray['filterValue']) continue
   
       if( filterArray[i][0] == '!' && filterArray[i][1]?.length > 0) {
-        console.log('passei aqq 1', filterArray[i])
         formattedFilters.push({ fieldName : '!', value:undefined })
         const fieldName = Array.isArray(filterArray[i][1]) ? filterArray[i][1][0] : filterArray[i]
         const value = Array.isArray(filterArray[i][1]) ? filterArray[i][1]['filterValue'] : filterArray['filterValue']
